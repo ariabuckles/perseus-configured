60139bd8df4b921fb8cd1c9d9c7fdde4ce84fa46
Matchu <matchu1993@gmail.com>
2021-07-13 20:50:53 -0700

    Work on React 17 compatibility
    I added the stuff I *think* we need for my app, and tried to get the demo app working…
    
    …but the demo app is now giving the same ref errors I thought I'd fixed in my app, by removing the React version conflicts. So I'm not sure if they're still a problem or what! Pushing this now to get started on testing, though.

--- src/perseus-globals.js
+++ src/perseus-globals.js
@@ -1,17 +1,25 @@
-import $ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import mathquill from 'mathquill';
-import katex from 'katex';
-import classnames from 'classnames';
-import _ from 'underscore';
+import $ from "jquery";
+import React from "react";
+import ReactDOM from "react-dom";
+import createReactClass from "create-react-class";
+import PropTypes from "prop-types";
+import mathquill from "mathquill";
+import katex from "katex";
+import classnames from "classnames";
+import _ from "underscore";
 
 // NOTE: We don't import the CSS here. Instead, applications can do it separately:
 //       import "perseus-configured/lib/khan-exercises.css";
 //       import "perseus-configured/lib/perseus.css";
 // TODO: Consider a build step to make it one CSS file, and include Mathquill too?
 
-window.jQuery = (window.$ = $);
+// Add some backfills to make React 17 behave like old-style React.
+// TODO: Update Perseus to use these libraries directly, instead. They're not perfect
+//       backfills and I'm not sure how reliable this is!
+React.createClass = createReactClass;
+React.PropTypes = PropTypes;
+
+window.jQuery = window.$ = $;
 window.katex = katex;
 window.mathquill = mathquill;
 window.classnames = classnames;
@@ -22,9 +30,9 @@ window._ = _;
 // load it in strict mode
 window.Raphael = undefined;
 
-const PureRenderMixin = require('react-addons-pure-render-mixin');
-const CSSTransitionGroup = require('react-addons-css-transition-group');
-const CreateFragment = require('react-addons-create-fragment');
+const PureRenderMixin = require("react-addons-pure-render-mixin");
+const CSSTransitionGroup = require("react-addons-css-transition-group");
+const CreateFragment = require("react-addons-create-fragment");
 
 window.React = React;
 window.ReactDOM = ReactDOM;
@@ -33,31 +41,31 @@ window.React.__internalAddons = {
   createFragment: CreateFragment,
 };
 
-window['underscore'] = window._;
-window['react'] = window.React;
-window['react-dom'] = window.ReactDOM;
-window['classnames'] = window.classNames;
-window['jquery'] = window.jQuery;
-window['react-addons-pure-render-mixin'] = PureRenderMixin;
-window['react-addons-css-transition-group'] = CSSTransitionGroup;
+window["underscore"] = window._;
+window["react"] = window.React;
+window["react-dom"] = window.ReactDOM;
+window["classnames"] = window.classNames;
+window["jquery"] = window.jQuery;
+window["react-addons-pure-render-mixin"] = PureRenderMixin;
+window["react-addons-css-transition-group"] = CSSTransitionGroup;
 
 /**
  * Sets up the basic environment for running Perseus in.
  */
 
 window.icu = {
-  getDecimalFormatSymbols: function() {
+  getDecimalFormatSymbols: function () {
     return {
-      decimal_separator: '.',
-      grouping_separator: ',',
-      minus: '-',
+      decimal_separator: ".",
+      grouping_separator: ",",
+      minus: "-",
     };
   },
 };
 
 window.KhanUtil = {
-  debugLog: function() {},
-  localeToFixed: function(num, precision) {
+  debugLog: function () {},
+  localeToFixed: function (num, precision) {
     return num.toFixed(precision);
   },
 };
@@ -66,22 +74,21 @@ window.Exercises = {
   localMode: true,
 
   useKatex: true,
-  khanExercisesUrlBase: '../',
+  khanExercisesUrlBase: "../",
 
-  getCurrentFramework: function() {
-    return 'khan-exercises';
+  getCurrentFramework: function () {
+    return "khan-exercises";
   },
   PerseusBridge: {
-    cleanupProblem: function() {
+    cleanupProblem: function () {
       return false;
     },
   },
 };
 
 window.Khan = {
-    Util: window.KhanUtil,
-    error: function() {},
-    query: {debug: ""},
-    imageBase: "/images/",
+  Util: window.KhanUtil,
+  error: function () {},
+  query: { debug: "" },
+  imageBase: "/images/",
 };
-
